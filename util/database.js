57de84ca7768dbfd33b7e815159056aba76cd350
Purushottam Balaka <122111835+purushottam-balaka@users.noreply.github.com>
2024-02-11 13:15:01 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize=require('sequelize');
-
-const sequelize=new Sequelize(process.env.DB_NAME,process.env.USER,process.env.PASSWORD,{
+require('dotenv).config();
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     dialect:'mysql',
     host:process.env.HOST,
 });
